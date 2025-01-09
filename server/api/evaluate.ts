02ab9837d5058630cb1d4c5eca760190ef69ec21
Axel <axelsorensenwork@gmail.com>
2025-01-10 00:08:02 +0100

    fixed system giving answers away

--- server/api/evaluate.ts
+++ server/api/evaluate.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 export default defineEventHandler(async (event) => {
     const body = await readBody(event);
     const instruction = 'Given a question and and the correct answer and a conversation history evaluate whether the user has demonstrated understanding of the answer through their replies.';
-    const criteria = 'If yes, return the text: Passed! Otherwise ask a question to help the user understand the answer better.';
+    const criteria = 'If yes, return the text: Passed! Otherwise ask a follow up question to help the user understand the question better. Never give the answer away to the user. Always refer to the original question';
     const prompt = `${instruction}\n\n${criteria}`;
     const openai = new OpenAI();
 
